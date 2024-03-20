3903994b10147534d5026abfd3245e67ea5388b5
todesking <discommunicative@gmail.com>
2024-03-20 19:11:08 +0900

    build.rs: better error messages

--- build.rs
+++ build.rs
@@ -1,30 +1,26 @@
-use std::{error::Error, path::Path, process::Command};
+use std::{os::unix::ffi::OsStrExt, path::Path, process::Command};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     let parser_dir = Path::new("tree-sitter-borlang");
 
-    build_parser(parser_dir, "program")?;
-    build_parser(parser_dir, "expr")?;
+    build_parser(parser_dir, "program");
+    build_parser(parser_dir, "expr");
 
     println!("cargo:rerun-if-changed={}", parser_dir.to_str().unwrap());
-
-    Ok(())
 }
 
-fn build_parser(base_dir: &Path, grammar_name: &str) -> Result<(), Box<dyn Error>> {
+fn build_parser(base_dir: &Path, grammar_name: &str) {
     let parser_dir = base_dir.join(format!("{grammar_name}_parser"));
 
-    std::fs::create_dir_all(&parser_dir)?;
+    std::fs::create_dir_all(&parser_dir).unwrap();
 
     let grammar_path = base_dir.join(format!("{grammar_name}_grammar.js"));
-    tree_sitter_cli_generate(&parser_dir, &grammar_path)?;
+    tree_sitter_cli_generate(&parser_dir, &grammar_path);
 
-    tree_sitter_build(&parser_dir, grammar_name)?;
-
-    Ok(())
+    tree_sitter_build(&parser_dir, grammar_name);
 }
 
-fn tree_sitter_cli_generate(dir: &Path, grammar_path: &Path) -> Result<(), Box<dyn Error>> {
+fn tree_sitter_cli_generate(dir: &Path, grammar_path: &Path) {
     let mut cmd = Command::new("npm");
     let cmd = cmd
         .current_dir(dir)
@@ -33,20 +29,19 @@ fn tree_sitter_cli_generate(dir: &Path, grammar_path: &Path) -> Result<(), Box<d
 
     println!("tree-sitter-cli generate {:?}", &cmd.get_args());
 
-    let out = cmd.output()?;
+    let out = cmd.output().unwrap();
+    println!("{}", String::from_utf8_lossy(&out.stdout));
+    eprintln!("{}", String::from_utf8_lossy(&out.stderr));
     if !out.status.success() {
-        return Err(format!(
-            "parser generation failed: stdout={}, stderr={}",
-            String::from_utf8_lossy(&out.stdout),
-            String::from_utf8_lossy(&out.stderr)
-        )
-        .into());
+        panic!(
+            "Parser generation failed: dir={}, grammar_path={}",
+            String::from_utf8_lossy(dir.as_os_str().as_bytes()),
+            String::from_utf8_lossy(grammar_path.as_os_str().as_bytes())
+        );
     }
-
-    Ok(())
 }
 
-fn tree_sitter_build(dir: &Path, grammar_name: &str) -> Result<(), Box<dyn Error>> {
+fn tree_sitter_build(dir: &Path, grammar_name: &str) {
     let src_dir = dir.join("src");
     let mut c_config = cc::Build::new();
     c_config.include(&src_dir);
@@ -57,5 +52,4 @@ fn tree_sitter_build(dir: &Path, grammar_name: &str) -> Result<(), Box<dyn Error
     let parser_path = src_dir.join("parser.c");
     c_config.file(&parser_path);
     c_config.compile(&format!("{grammar_name}_parser"));
-    Ok(())
 }
